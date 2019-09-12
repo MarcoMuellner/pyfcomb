c601a5f70f92c828066d4fc40869cd19d6f8ea3d
mm7490 <muellnermarco@gmail.com>
2019-09-12 22:06:05 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="pyfcomb",
-    version="1.0.0",
+    version="1.0.1",
     author="Marco Müllner",
     author_email="muellnermarco@gmail.com",
     description="Computing frequency combinations made simple",
